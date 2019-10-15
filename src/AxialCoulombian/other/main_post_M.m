1e84d9f1ddf6e33c75109e0f91f1a88c3ce3673c
UniPD-DII-ETCOMP <55701889+UniPD-DII-ETCOMP@users.noreply.github.com>
2019-10-15 13:22:31 +0200

    removed unused directory
    removed unused directory

--- src/AxialCoulombian/other/main_post_M.m
+++
@@ -1,6 +0,0 @@
-% 
-if ex.vol_mag
-[sol.M_r,sol.M_i,sol.M_norm_r,sol.M_norm_i,sol.M_bar] = fun_post_J_Quad_face2(NaN,N.face_mag,Matrix_P0_m,F1_m,C1_m,sol.m,Fac_Ed_loc_m,Aed_m,Led_m);
-sol.M_r=-sol.M_r;
-sol.M_i=-sol.M_i;
-end
